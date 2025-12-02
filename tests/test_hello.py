ebfd621f873c2b5ac38fc7aad457fd9cb0523130
Dawid <153355666+NapBon2003@users.noreply.github.com>
2025-12-02 13:35:52 +0100

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,4 +1,9 @@
 from fastapi.testclient import TestClient
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 from main import app
 
 def test_hello():
